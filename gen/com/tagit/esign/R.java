94a10f4e4b959db78eaece4ee850e2f8538865a1
tagitdev <tagitdev@192.168.18.18>
2014-11-13 15:35:52 +0800

    Working directory of NRIC Front and Back view

--- gen/com/tagit/esign/R.java
+++ gen/com/tagit/esign/R.java
@@ -27,24 +27,46 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int imageView1=0x7f080002;
-        public static final int imageView2=0x7f080003;
-        public static final int imageView3=0x7f080004;
-        public static final int imageView4=0x7f080005;
-        public static final int img_nric=0x7f080001;
+        public static final int action_cancel=0x7f080012;
+        public static final int action_choose=0x7f080011;
+        public static final int action_settings=0x7f080013;
+        public static final int action_take_photo=0x7f080010;
+        public static final int id_static_main_nric_back_view=0x7f080006;
+        public static final int id_static_main_nric_front_view=0x7f080001;
+        public static final int id_static_main_sign_view=0x7f08000b;
+        public static final int img_main_delete=0x7f080004;
+        public static final int img_main_delete2=0x7f080009;
+        public static final int img_main_delete3=0x7f08000e;
+        public static final int img_nric=0x7f080002;
+        public static final int img_nric_back=0x7f080007;
+        public static final int img_sign=0x7f08000c;
         public static final int textView1=0x7f080000;
+        public static final int txt_main_error_nric_back_view=0x7f08000a;
+        public static final int txt_main_error_nric_view=0x7f080005;
+        public static final int txt_main_error_sign=0x7f08000f;
+        public static final int txt_main_nric_back_desc=0x7f080008;
+        public static final int txt_main_nric_desc=0x7f080003;
+        public static final int txt_main_sign_desc=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int context_menu=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int id_action_cancel=0x7f05000a;
+        public static final int id_action_choose=0x7f050009;
+        public static final int id_action_take_photo=0x7f050008;
+        public static final int txt_static_main_click_title=0x7f050005;
+        public static final int txt_static_main_nric_back_view_title=0x7f050006;
+        public static final int txt_static_main_nric_front_view_title=0x7f050004;
+        public static final int txt_static_main_sign_view_title=0x7f050007;
+        public static final int txt_static_main_upload_title=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/tagit/esign/MainActivity.java
+++ src/com/tagit/esign/MainActivity.java
@@ -3,124 +3,107 @@ package com.tagit.esign;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import android.R.drawable;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.AnimatorRes;
-import android.support.annotation.DrawableRes;
 import android.util.Base64;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	static final int REQUEST_IMAGE_CAPTURE = 1;
-	ImageView img1;
-	ImageView img4;
-	ImageView imgDecode;
+	static final int REQUEST_CHOOSE_LIBRARY = 2;
+	static final CharSequence[] items = {"Take a photo","Choose from library","Cancel"};
+	static final String format = "added on "+ new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+	ImageView img_nric;
+	ImageView img_nric_back;
+	ImageView img_sign;
+	ImageView nric_delete;
+	ImageView nric_back_delete;
+	ImageView sign_delete;
+	TextView nric_desc;
+	TextView nric_back_desc;
+	TextView sign_desc;
+	int selectedViewID = 0;
+	TextView nric_error;
+	TextView nric_back_error;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ImageView img_nric = (ImageView) findViewById(R.id.img_nric);
-		img1 = (ImageView) findViewById(R.id.imageView1);
-		img4 = (ImageView) findViewById(R.id.imageView4);
-		imgDecode = (ImageView) findViewById(R.id.imageView2);
-		ImageView img3 = (ImageView) findViewById(R.id.imageView3);
 		
-		img3.setOnClickListener(new OnClickListener() {
+		
+		img_nric = (ImageView) findViewById(R.id.img_nric);
+		img_nric_back = (ImageView) findViewById(R.id.img_nric_back);
+		img_sign = (ImageView) findViewById(R.id.img_sign);
+		
+		nric_delete = (ImageView) findViewById(R.id.img_main_delete);
+		nric_back_delete = (ImageView) findViewById(R.id.img_main_delete2);
+		sign_delete = (ImageView) findViewById(R.id.img_main_delete3);
+		
+		nric_desc = (TextView) findViewById(R.id.txt_main_nric_desc);
+		nric_back_desc = (TextView) findViewById(R.id.txt_main_nric_back_desc);
+		sign_desc = (TextView) findViewById(R.id.txt_main_sign_desc);
+		
+		nric_error = (TextView) findViewById(R.id.txt_main_error_nric_view);
+		nric_back_error = (TextView) findViewById(R.id.txt_main_error_nric_back_view);
+		
+		
+		img_nric.setOnClickListener(this);
+		img_nric_back.setOnClickListener(this);
+		img_sign.setOnClickListener(this);
+	
+		nric_delete.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				img1.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_camera));
+				img_nric.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_camera));
+				nric_desc.setText(R.string.txt_static_main_click_title);
+				nric_delete.setVisibility(View.INVISIBLE);
+				nric_error.setText("deleted on "+format);
 			}
 		});
-		
-		img_nric.setOnClickListener(new OnClickListener() {
+		nric_back_delete.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-				        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-				 }
+				img_nric_back.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_camera));
+				nric_back_desc.setText(R.string.txt_static_main_click_title);
+				nric_back_delete.setVisibility(View.INVISIBLE);
+				nric_back_error.setText("deleted on "+format);
 			}
 		});
 		
-		img4.setOnClickListener(new OnClickListener() {
+		sign_delete.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(Intent.ACTION_PICK);
-				intent.setType("image/*");
-				startActivityForResult(Intent.createChooser(intent, "Select Pic"),2);
+				img_sign.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_camera));
 			}
-		});
-		
+		});			
 	}
 	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-	        Bundle extras = data.getExtras();
-	        Bitmap imageBitmap = (Bitmap) extras.get("data");
-	        
-	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	        boolean compress = imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-	        if (compress) {
-	        	String encode = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
-			}
-	        img1.setImageBitmap(imageBitmap);
-	    }
-		
-		if (requestCode == 2 && resultCode == RESULT_OK) {
-			Uri data2 = data.getData();
-			InputStream imageStream = null;
-			try {
-				imageStream = getContentResolver().openInputStream(data2);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
-	            
-	            
-//			Uri data2 = data.getData();
-//			Log.d("Data2", data2.toString());
-//			String[] filePathColoum = {MediaStore.Images.Media.DATA};
-//			
-//			Cursor cursor = getContentResolver().query(
-//					data2, filePathColoum, null, null, null);
-//		 cursor.moveToFirst();
-//		
-//		 int columnIndex = cursor.getColumnIndex(filePathColoum[0]);
-//		 String filePath = cursor.getString(columnIndex);
-//		 cursor.close();
-//
-//
-//		 Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
-		 img4.setImageBitmap(yourSelectedImage);
-		}
-	}
-
 	private void decode(String encode) {
 		// TODO Auto-generated method stub
 		byte[] decode = Base64.decode(encode, Base64.DEFAULT);
-		imgDecode.setImageBitmap(BitmapFactory.decodeByteArray(decode, 0, decode.length));
+//		imgDecode.setImageBitmap(BitmapFactory.decodeByteArray(decode, 0, decode.length));
 	}
 
 	@Override
@@ -132,13 +115,109 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
+
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public void onClick(final View v) {
+		// TODO Auto-generated method stub
+		selectedViewID = v.getId();
+		Builder builder = new AlertDialog.Builder(MainActivity.this);
+		builder.setTitle("Choose");
+		builder.setItems(items, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int item) {
+				// TODO Auto-generated method stub
+				switch (item) {
+				case 0:
+					Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+					if (i.resolveActivity(getPackageManager()) != null) {
+					        startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
+					}
+					break;
+				case 1:
+					Intent intent = new Intent(Intent.ACTION_PICK);
+					intent.setType("image/*");
+					startActivityForResult(Intent.createChooser(intent, "Select Pic"),REQUEST_CHOOSE_LIBRARY);
+					break;
+					
+				case 2 : 
+					 
+					break;
+				default:
+					break;
+				}
+				
+			}
+		});
+		
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	       
+	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	        boolean compress = imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+	        if (compress) {
+	        	String encode = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+			}
+	        fillImage(imageBitmap);
+	    }
+		
+		if (requestCode == REQUEST_CHOOSE_LIBRARY && resultCode == RESULT_OK) {
+			Uri data2 = data.getData();
+			Bundle extras = data.getExtras();
+			
+			InputStream imageStream = null;
+			try {
+				imageStream = getContentResolver().openInputStream(data2);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        Bitmap imageBitmap = BitmapFactory.decodeStream(imageStream);
+	        
+	        fillImage(imageBitmap);
+		}
+	}
+	
+	private void fillImage(Bitmap bitmap) {
+		
+		if (selectedViewID != 0) {
+			
+			switch (selectedViewID) {
+			case R.id.img_nric:
+					img_nric.setImageBitmap(bitmap);
+					nric_desc.setText(format);
+					nric_delete.setVisibility(View.VISIBLE);
+					nric_error.setVisibility(View.VISIBLE);
+				break;
+			case R.id.img_nric_back:
+					img_nric_back.setImageBitmap(bitmap);
+					nric_back_desc.setText(format);
+					nric_back_delete.setVisibility(View.VISIBLE);
+					nric_back_error.setVisibility(View.VISIBLE);
+				break;
+			case R.id.img_sign:
+				
+				break;
+			default:
+				selectedViewID = 0;
+				break;
+			}
+		}
+	}
+
 }
